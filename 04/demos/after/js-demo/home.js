380574eef1098bab41776ac61246cd47baf3f1ad
Rajesh7889 <khajuriarajesh768@gmail.com>
2023-04-27 13:11:59 +0530

    all about variables

--- 04/demos/after/js-demo/home.js
+++ 04/demos/after/js-demo/home.js
@@ -1,7 +1,40 @@
 
+//variable use to identify a memory
+// location in the memory.
+//used to store data..
+
+//It is how we can declare a variable..
 
 
+//let is a keyword//
 let price = 25;
+let total_iteams = 2;
+//we can assign a string value..
+let product = 'bat';
+//we can also assign a boolen value to a variable/
+let discount = true;
+// naming conventions
+/* must be defined and declared and initialised
+   there should not spacing
+   should start with(_ $ letter)
+   should not be a keyboard..
+   name should be close to thing we are going to store in it
+   it will make easy to understand what are we saving in it.. */
+
+//changing variable values
+//total_iteams = 4;
+//constant variable once set, can't be changed..
+//must set a value while declaring a constant.. 
+const sale = "monday";
+ //assigning value to a constant twice is illegal..
+ /* similar to let keyword , we can also use var keyword to 
+    declare a variable but there is a problem while 
+    using var variable , it is, if we use the variable 
+    before it's declaration and initialisation , it return undefined \
+    variable ..*/
+    // syntax => var price = 44;
+
 
 showMessage(price);
 console.log(price);
+
