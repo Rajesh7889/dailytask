a846d58ee6bddfd1c5e788c6ab4d5ffbae8fe28d
Rajesh7889 <khajuriarajesh768@gmail.com>
2023-04-29 00:17:49 +0530

    functions

--- 07/demos/after/js-demo/home.js
+++ 07/demos/after/js-demo/home.js
@@ -1,3 +1,34 @@
-
+//functions in javascript..
+function userName(){   //declaring and defining a function..
+  //  console.log("rajesh");
+}
 changePercentOff( 32 );
+
+userName(); // calling a function..
+let fn = function(){
+     //  console.log("rajesh");
+}
+fn();
+
+//passing arguement to function...
+let fxn = function(message){
+   // console.log(message);
+}
+fxn("hello");
+
+//function may return value...
+let fun=function(value){
+    let code = value * 4;
+    return code;//returning a value...
+}
+//console.log(fun(4));//return 16..
+ //showMessage(fun(4));
+
+//function scope...local variable and methods have scope within the blocks of function...
+let fsn = function(value){
+    let code = value * 38;
+    return code;
+}
+let cod = fsn(4);
+console.log(code);// code not defined error..
 
